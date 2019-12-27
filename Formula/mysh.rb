d100d94493d00d3ab5671ab98e22c161cf25a365
mishamyrt <misha@myrt.co>
2019-12-27 14:48:29 +0000

    Release 0.1.1

--- Formula/mysh.rb
+++ Formula/mysh.rb
@@ -1,9 +1,9 @@
 class Mysh < Formula
     desc "A tool for improving SSH user experience"
     homepage "https://github.com/mishamyrt/mysh/"
-    url "https://github.com/mishamyrt/mysh/releases/download/v0.1.0/mysh_0.1.0_darwin_amd64.tar.gz"
-    version "0.1.0"
-    sha256 "f034bf2dd1b56126f4af72e782acfaa18e386506bae626006a75924983bf1663"
+    url "https://github.com/mishamyrt/mysh/releases/download/v0.1.1/mysh_0.1.1_darwin_amd64.tar.gz"
+    version "0.1.1"
+    sha256 " 07c7097252eee39b1b6895a470458bf6a9bee4e64305f89dbe2104d6ff437445"
   
     def install
       bin.install "mysh"
